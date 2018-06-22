43b5fe9fea093e7f5cd7424bf53a60da50638e6c
木原大輝 <taiki@diffeasy.com>
2018-06-22 14:36:19 +0900

    fix: CORS設定追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:80', '127.0.0.1:80', 'localhost:3000', '127.0.0.1:3000'
+    origins 'localhost:80', '127.0.0.1:80', 'localhost:3000', '127.0.0.1:3000', ENV['CORS_FRONT_URL']
 
     resource '*',
       headers: :any,
